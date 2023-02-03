176da5602096217ea734faf3b030dde64dbc02a6
alexveb <verbalex2011@hotmail.com>
2023-02-03 09:46:43 +0200

    Fix Header Img and Resize

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <img src="images/header-img.jpg" className="header-img" />
+        </div>
+    );
+}
+
+export default Header;
